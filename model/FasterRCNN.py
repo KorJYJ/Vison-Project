c1853587ed9047008b610246f6bff546548ff379
KorJYJ <tmspr3693@naver.com>
2023-09-05 01:24:19 +0900

    faster rcnn

--- model/FasterRCNN.py
+++ model/FasterRCNN.py
@@ -56,8 +56,8 @@ class RPN(nn.Module):
         img_height, img_width = image_size
         grid_height, grid_width = grid_size
         
-        x_range = torch.range(0, img_width, img_width / (grid_width - 1))
-        y_range = torch.range(0, img_height, img_height / (grid_height - 1))
+        x_range = torch.range(0, img_width, img_width / grid_width / 2)[range(1, grid_width * 2, 2)]
+        y_range = torch.range(0, img_height, img_height / grid_height / 2)[range(1, grid_height * 2, 2)]
         
         grid_x, grid_y = torch.meshgrid(x_range, y_range)
         
@@ -78,6 +78,9 @@ class RPN(nn.Module):
                 anchor_list.append(xywh)
                 
         anchor_boxes = torch.concat(anchor_list, dim = 2)
+        anchor_boxes[..., 0] -= anchor_boxes[..., 2] / 2
+        anchor_boxes[..., 1] -= anchor_boxes[..., 3] / 2
+
         anchor_boxes[..., 2:] += anchor_boxes[..., :2]
         return anchor_boxes
     
@@ -86,13 +89,10 @@ class RPN(nn.Module):
         anchor = anchor.clone().cpu().numpy()
         target = target.clone().cpu().numpy()
         
-        # 이미지 밖으로 box가 나자기 않도록 만들기
-
         _ious = ious(target, anchor)
-
         pos_target, pos_anc = np.where(_ious > 0.7)
         _, neg_anc = np.where(_ious < 0.3)
-
+        negative_pedding = 0
         if len(pos_anc) > 128:
             positive_sample_index = np.random.choice(range(len(pos_anc)), random_choice, replace = False)
         else :
@@ -109,22 +109,27 @@ class RPN(nn.Module):
         batch = x.shape[0]
         grid_x = x.shape[-1]
         grid_y = x.shape[-2]
-        
+        height, width = img_size
         x = self.conv(x)
         bbox_regressor = self.bbox_layer(x) # dx, dy, dw, dh || [gird_y, grid_x, 9 * 4]
         score_proposal = self.score_layer(x) # [gird_y, grid_x, 9 * 2]
 
-        bbox_regressor = bbox_regressor.view(batch, grid_y, grid_x, 9, 4)
-        score_proposal = score_proposal.view(batch, grid_y, grid_x, 9, 2)
-        anchor_box = self.make_anchor_box(img_size, [grid_y, grid_x]).cuda()
+        # bbox_regressor = bbox_regressor.view(batch, grid_y, grid_x, 9, 4)
+        # score_proposal = score_proposal.view(batch, grid_y, grid_x, 9, 2)
 
+        bbox_regressor = bbox_regressor.view(-1, 4)
+        score_proposal = score_proposal.view(-1, 2)
+        anchor_box = self.make_anchor_box(img_size, [grid_y, grid_x]).cuda()
+        anchor_box = anchor_box.view(-1, 4)
+
+        bbox =self.box_decoder(bbox_regressor, anchor_box)
+        bbox, scores = self.proposal_filter(bbox, score_proposal, img_size)
         loss = None
 
-        if target is not None:
-            anchor_box = anchor_box.view(-1, 4)
+        if self.training:
             target = target.view(-1, 4)
-            bbox_regressor = bbox_regressor.view(-1, 4)
-            score_proposal = score_proposal.view(-1, 2)
+            # bbox_regressor = bbox_regressor.view(-1, 4)
+            # score_proposal = score_proposal.view(-1, 2)
 
             pos_taget_idx, pos_anc_idx, neg_anc_idx = self.pos_neg_sampling(anchor_box, target)
             
@@ -134,17 +139,17 @@ class RPN(nn.Module):
             score_proposal = score_proposal[pos_neg_idx]
             pos_anchor_box = anchor_box[pos_anc_idx]
             target = target[pos_taget_idx]
+
             target_scores = torch.zeros_like(score_proposal)
             target_scores[:len(pos_anc_idx), 0] = 1
             target_scores[len(pos_anc_idx):, 1] = 1
-
             target_regressor = self.box_encode(pos_anchor_box, target)
 
             loss = self.compute_loss(bbox_regressor, score_proposal, target_regressor, target_scores)
 
-        return loss
+        return bbox, scores, loss
 
-    def box_encode(self, anchor, bbox):
+    def box_encode(self, bbox, anchor):
         anchor[:, 2:] -= anchor[:, :2] # [x, y, w, h]
         bbox[:, 2:] -= bbox[:, :2] # [x, y, w, h]
 
@@ -156,7 +161,66 @@ class RPN(nn.Module):
         dw = torch.log(bbox[:, 2:3]/anchor[:, 2:3])
         dh = torch.log(bbox[:, 3:4]/anchor[:, 3:4])
         target_regressor = torch.concat([dx, dy, dw, dh], dim = 1)
+
         return target_regressor
+    
+
+    def box_decoder(self, bbox_regressor, anchor):
+        w_a = (anchor[:, 2] - anchor[:, 0])
+        h_a = (anchor[:, 3] - anchor[:, 1])
+        cx_a = (anchor[:, 0] + anchor[:, 2])/2
+        cy_a = (anchor[:, 1] + anchor[:, 3])/2
+
+        cx = bbox_regressor[:, 0] * w_a + cx_a
+        cy = bbox_regressor[:, 1] * h_a + cy_a
+        w = torch.exp(bbox_regressor[:, 2]) * w_a
+        h = torch.exp(bbox_regressor[:, 3]) * h_a
+
+        x1 = cx - w/2
+        y1 = cy - h/2
+        x2 = cx + w/2
+        y2 = cy + h/2
+
+        bbox = torch.cat([x1.reshape(-1, 1), y1.reshape(-1, 1), x2.reshape(-1, 1), y2.reshape(-1, 1)], dim= 1)
+
+        return bbox
+    
+    def proposal_filter(self, proposals, scores, img_size):
+        height, width = img_size
+        proposals = proposals.clone()
+        scores = scores.clone()
+        if proposals.shape[0] > 200000:
+            _, topk = torch.topk(scores[:, 0], 200000)
+            proposals = proposals[topk]
+            scores = scores[topk]
+
+        # 이미지 밖으로 box가 나자기 않도록 만들기
+        proposals[..., [1, 3]] = torch.clamp(proposals[..., [1, 3]], max=height)
+        proposals[..., [1, 3]] = torch.clamp(proposals[..., [1, 3]], min=0)
+        proposals[..., [0, 2]] = torch.clamp(proposals[..., [0, 2]], max=width)
+        proposals[..., [0, 2]] = torch.clamp(proposals[..., [0, 2]], min=0)
+        
+        keep = scores[:, 0] > 0.2
+        proposals = proposals[keep]
+        scores = scores[keep]
+
+        # box 사이즈가 너무 작으면 없애기
+        area = torch.abs((proposals[..., 0] - proposals[..., 2]) * (proposals[..., 1]  - proposals[..., 3]))
+        keep = area > 100
+        
+        proposals = proposals[keep]
+        scores = scores[keep]
+
+        nms_index = torchvision.ops.nms(proposals, scores[:, 0], iou_threshold= 0.7)
+        nms_index = nms_index[:2000]
+
+        bbox = proposals[nms_index]
+        scores = scores[nms_index]
+
+    
+
+        return bbox, scores
+
 
     def compute_loss(self, 
              bbox_regressor : torch.Tensor, 
@@ -188,36 +252,67 @@ class RegionProposal(nn.Module):
 class MyFasterRCNN(nn.Module):
     def __init__(self, n_classes):
         super(MyFasterRCNN, self).__init__()
+
         self.fc_layer = nn.Sequential(
-            nn.Linear(25088, 4096),
-            nn.BatchNorm1d(4096),
+            nn.Linear(2048, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
             nn.ReLU(),
         )
 
-        self.classifier = nn.Linear(4096, n_classes)
-        self.bounding_box = nn.Linear(4096, 4)
+        self.classifier = nn.Linear(512, n_classes)
+        self.bounding_box = nn.Linear(512, 4)
 
     def forward(self, x):
+        batch = x.shape[0]
+        x = x.view(batch, -1)
+        print(x.shape)
         x = self.fc_layer(x)
         cls = self.classifier(x)
         bbox = self.bounding_box(x)
 
         return cls, bbox
+    
+class RoIPool(nn.Module):
+    def __init__(self, output_size):
+        super(RoIPool, self).__init__()
+        self.max_pool = nn.AdaptiveMaxPool2d(output_size)
+        
+
+    def forward(self, feature, boxes, image_size):
+        image_height, image_width = image_size
+
+        feature_height, feature_width = feature.shape[-2], feature.shape[-1]
+        output = []
+        # boxes[:, :2] -= boxes[:, 2:] / 2
+        # boxes[:, 2:] += boxes[:, :2]
+        boxes[:, [0, 2]] *= feature_width/image_width
+        boxes[:, [1, 3]] *= feature_height/image_height
+        for box in boxes:
+            x1, y1, x2, y2 = box.int()
+            output.append(self.max_pool(feature[..., y1: y2, x1:x2]))
+
+        output = torch.stack(output)
+
+        return output
 
 def train():
     mean = [0.485, 0.456, 0.406]
     std = [0.229, 0.224, 0.225]
-    vgg = vgg16(weights = torchvision.models.VGG16_Weights.IMAGENET1K_V1).features.cuda()
+    vgg = vgg16(weights = torchvision.models.VGG16_Weights.IMAGENET1K_V1).features.cuda()[:-1]
     rpn = RPN(512).cuda()
-
+    roi_pool = RoIPool((2, 2)).cuda()
+    roi_head = MyFasterRCNN(2).cuda()
     faster_rcnn = nn.ModuleList([vgg, rpn])
 
-    img = cv2.imread("D:\\datasets\\VOCdevkit\\VOC2012\\JPEGImages\\2007_004538.jpg")
+    img = cv2.imread("D:\\datasets\\VOCdevkit\\VOC2012\\JPEGImages\\2007_002597.jpg")
         
     h, w, _ = img.shape
     scale = 600/min(h, w)
 
-    target_bboxes, target_labels = VOC_bbox("D:\\datasets\\VOCdevkit\\VOC2012\\Annotations\\2007_004538.xml")
+    target_bboxes, target_labels = VOC_bbox("D:\\datasets\\VOCdevkit\\VOC2012\\Annotations\\2007_002597.xml")
     target_bboxes = torch.tensor(target_bboxes).cuda()
     # target_bboxes[:, 2:] -= target_bboxes[:, :2]
     target_bboxes *= scale
@@ -231,16 +326,24 @@ def train():
     
     img = transforms.Normalize(mean, std)(img).float().cuda()
 
-    optimizer  = torch.optim.SGD(faster_rcnn.parameters(), lr = 1e-4, momentum=0.9)
-    
+    optimizer  = torch.optim.Adam(faster_rcnn.parameters(), lr = 1e-4)
 
-    for epoch in range(50):
+    for epoch in range(800):
         vgg_feature = vgg(img)
-        loss = rpn(vgg_feature, (h, w), target_bboxes)
+        region_proposal, scores, loss = rpn(vgg_feature, (h, w), target_bboxes)
+        
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-        print(f"{epoch} : loss={loss}")
+        print(f"{epoch} : loss={loss}, proposal : {region_proposal.shape}")
+
+        # freezing rpn
+        rois = roi_pool(vgg_feature, region_proposal, (h, w))
+        b_class,bbox = roi_head(rois)
+        print(bbox)
+
+
+
     #bbox_proposal : [1, x_axis, y_axis, 9, 4]
     
     # bbox_proposal = bbox_proposal.view(-1, 4)
